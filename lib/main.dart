d074f3b83b3fb281cf1c9c8f76353eb6d0b9ef9d
João Gabriel <joaogabrielmoura14@gmail.com>
2022-04-03 00:02:46 -0300

    Erro corrigido

--- lib/main.dart
+++ lib/main.dart
@@ -33,17 +33,17 @@ class _HomeState extends State<Home> {
       double height = double.parse(heightController.text) / 100;
       double imc = weigth / (height * height);
       if (imc < 18.6) {
-        _infoText = "Abaixo do peso ({$imc.toStringAsPrecision(3)})";
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso ideal ({$imc.toStringAsPrecision(3)}})";
+        _infoText = "Peso ideal (${imc.toStringAsPrecision(3)}})";
       } else if (imc >= 24.9 && imc < 29.9) {
         _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(3)}})";
       } else if (imc >= 29.9 && imc < 39.9) {
-        _infoText = "Obesidade  grau I ({$imc.toStringAsPrecision(3)}})";
+        _infoText = "Obesidade  grau I (${imc.toStringAsPrecision(3)}})";
       } else if (imc >= 34.9 && imc < 39.9) {
-        _infoText = "Obesidade grau II ({$imc.toStringAsPrecision(3)}})";
+        _infoText = "Obesidade grau II (${imc.toStringAsPrecision(3)}})";
       } else if (imc >= 40) {
-        _infoText = "Obesidade grau III ({$imc.toStringAsPrecision(3)}})";
+        _infoText = "Obesidade grau III (${imc.toStringAsPrecision(3)}})";
       }
     });
   }
